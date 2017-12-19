80c4f40d67a2fd319555c82e1150cac7fe4fc979
Pierre-Hugues Husson <phh@phh.me>
2017-12-19 02:01:32 +0100

    We also dont want WebViewGoogle

--- tmp/Android.mk
+++ tmp/Android.mk
@@ -3,7 +3,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := phh-overrides
 PACKAGES.phh-overrides.OVERRIDES := \
-	webview
+	webview \
+	WebViewGoogle
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
