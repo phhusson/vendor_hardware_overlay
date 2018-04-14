37f0fc7cd6eb10bd34b217e9ed0126b4771afd88
Pierre-Hugues Husson <phh@phh.me>
2018-04-14 16:43:00 +0200

    Change Picker from privileged to system-signed for easier permissions

--- Picker/Android.mk
+++ Picker/Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
+LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PACKAGE_NAME := HardwareOverlayPicker
 LOCAL_PROGUARD_ENABLED := disabled
