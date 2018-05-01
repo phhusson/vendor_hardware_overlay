e7df1af1eaa8fb129990b88436497469c30d12b7
Pierre-Hugues Husson <pierre-hugues.husson@softathome.com>
2018-05-01 20:45:25 +0200

    Include fingerprint.kl for Huawei

--- overlay.mk
+++ overlay.mk
@@ -10,5 +10,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	hw-fpnav-daemon
 PRODUCT_COPY_FILES += \
+	vendor/hardware_overlay/Huawei/Fingerprint/hw-fingerprint.rc:system/etc/init/hw-fingerprint.rc \
 	vendor/hardware_overlay/Huawei/Fingerprint/hw-fpnav:system/bin/hw-fpnav \
-	vendor/hardware_overlay/Huawei/Fingerprint/hw-fpnav.dex:system/phh/hw-fpnav.dex
+	vendor/hardware_overlay/Huawei/Fingerprint/hw-fpnav.dex:system/phh/hw-fpnav.dex \
+	vendor/hardware_overlay/Huawei/Fingerprint/fingerprint.kl:system/phh/huawei/fingerprint.kl
