02cd4c4f194c744d6bf3d247337bc4c61e9245a7
Pierre-Hugues Husson <phh@phh.me>
2018-07-22 21:18:16 +0200

    Revert "Add "Touchscreen" app to properly setup touchscreen calibration"
    This reverts commit 19f71681a460a1335c631fd2d096d90a42bfaa2b.
    
    This is no longer needed since ro.sf.hwrotation has been merged in
    frameworks/native based on
    https://android-review.googlesource.com/c/platform/frameworks/native/+/671042

--- overlay.mk
+++ overlay.mk
@@ -20,8 +20,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-xiaomi-mimix2s \
 	treble-overlay-xiaomi-redmi6pro \
 	HardwareOverlayPicker \
-	QtiAudio \
-	Touchscreen
+	QtiAudio
 
 
 PRODUCT_PACKAGES += \
