9dc811cfc46666c8a40cca58f16a42ae8b20b450
Pierre-Hugues Husson <phh@phh.me>
2018-08-12 22:14:59 +0200

    Use hwbinder libs instead of hardcoding java code

--- Qualcomm/QtiAudio/Android.mk
+++ Qualcomm/QtiAudio/Android.mk
@@ -4,6 +4,10 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
+LOCAL_STATIC_JAVA_LIBRARIES := \
+	vendor.qti.hardware.radio.am-V1.0-java-static \
+	vendor.qti.qcril.am-V1.0-java-static
+
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PACKAGE_NAME := QtiAudio
