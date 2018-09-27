c5d9b7a335f88963c911eb93b6a99b806d5cc5c1
Pierre-Hugues Husson <phh@phh.me>
2018-09-27 14:03:37 +0200

    Delete hw-fpnav. This is now handled by Treble app

--- overlay.mk
+++ overlay.mk
@@ -109,13 +109,3 @@ PRODUCT_PACKAGES += \
 	treble-overlay-xiaomi-redminote8pro \
 	treble-overlay-xiaomi-redmis2 \
 
-
-PRODUCT_PACKAGES += \
-	hw-fpnav-daemon \
-
-PRODUCT_COPY_FILES += \
-	vendor/hardware_overlay/Huawei/Fingerprint/hw-fingerprint.rc:system/etc/init/hw-fingerprint.rc \
-	vendor/hardware_overlay/Huawei/Fingerprint/hw-fpnav:system/bin/hw-fpnav \
-	vendor/hardware_overlay/Huawei/Fingerprint/hw-fpnav.dex:system/phh/hw-fpnav.dex \
-	vendor/hardware_overlay/Huawei/Fingerprint/fingerprint.kl:system/phh/huawei/fingerprint.kl \
-
