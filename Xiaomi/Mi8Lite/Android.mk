77d458f69f2d9bd12c99d92c68bc3940c26adae1
suwakowww <495599914@qq.com>
2018-11-12 12:08:56 +0800

    fix wrong name

--- Xiaomi/Mi8Lite/Android.mk
+++ Xiaomi/Mi8Lite/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_PACKAGE_NAME := treble-overlay-xiaomi-mi6x
+LOCAL_PACKAGE_NAME := treble-overlay-xiaomi-mi8lite
 LOCAL_MODULE_PATH := $(TARGET_OUT)/overlay
 LOCAL_IS_RUNTIME_RESOURCE_OVERLAY := true
 LOCAL_PRIVATE_PLATFORM_APIS := true
--- Xiaomi/Mi8UD/Android.mk
+++ Xiaomi/Mi8UD/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_PACKAGE_NAME := treble-overlay-xiaomi-mi8ee
+LOCAL_PACKAGE_NAME := treble-overlay-xiaomi-mi8ud
 LOCAL_MODULE_PATH := $(TARGET_OUT)/overlay
 LOCAL_IS_RUNTIME_RESOURCE_OVERLAY := true
 LOCAL_PRIVATE_PLATFORM_APIS := true
