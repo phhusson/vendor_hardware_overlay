054318aa2b4b4b96519a6bbb0f549aed3188b313
Tadi <37599238+TadiT7@users.noreply.github.com>
2019-02-06 18:53:40 -0500

    Update HTC/ime/Android.mk
    Co-Authored-By: jeanrivera <jeanrivera@gmail.com>

--- HTC/ime/Android.mk
+++ HTC/ime/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_PACKAGE_NAME := treble-overlay-htc-ime
+LOCAL_PACKAGE_NAME := treble-overlay-htc-u12plus
 LOCAL_MODULE_PATH := $(TARGET_OUT)/overlay
 LOCAL_IS_RUNTIME_RESOURCE_OVERLAY := true
 LOCAL_PRIVATE_PLATFORM_APIS := true
