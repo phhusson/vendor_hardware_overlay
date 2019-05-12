fbcf81ba78ecc2e67abe36c6c7081dc94daf6604
GodcraftPlata <49221554+GodcraftPlata@users.noreply.github.com>
2019-05-11 22:22:56 -0500

    Update overlay.mk

--- overlay.mk
+++ overlay.mk
@@ -33,8 +33,6 @@ PRODUCT_PACKAGES += \
 	treble-overlay-lenovo-z5 \
 	treble-overlay-lg-g7 \
 	treble-overlay-lg-v40 \
-	treble-overlay-moto-e5 \
-	treble-overlay-moto-e5play \
 	treble-overlay-moto-e5plus \
 	treble-overlay-moto-g6 \
 	treble-overlay-moto-g6plus \
