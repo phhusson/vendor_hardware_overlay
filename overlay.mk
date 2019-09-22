42d517e61b622d39ff67459d0d5f1d8b4728beda
deanlancer <45734187+deanlancer@users.noreply.github.com>
2019-09-22 19:21:15 +0300

    Update overlay.mk

--- overlay.mk
+++ overlay.mk
@@ -1,13 +1,11 @@
-
-
-
+PRODUCT_PACKAGES += \
 	HardwareOverlayPicker \
-	hw-fpnav-daemon \
-PRODUCT_COPY_FILES += \
-PRODUCT_PACKAGES += \
-PRODUCT_PACKAGES += \
 	QtiAudio \
 	TrebleApp \
+	treble-overlay-NavBar \
+	treble-overlay-NightMode \
+	treble-overlay-SystemUI-FalseLocks \
+	treble-overlay-Telephony-LTE \
 	treble-overlay-asus-zenfone5 \
 	treble-overlay-asus-zenfone5z \
 	treble-overlay-asus-zenfonelivel1za550kl \
@@ -30,11 +28,11 @@ PRODUCT_PACKAGES += \
 	treble-overlay-huawei-PRA \
 	treble-overlay-huawei-RNE \
 	treble-overlay-huawei-STF \
-	treble-overlay-huawei-telephony \
 	treble-overlay-huawei-VTR \
 	treble-overlay-huawei-WAS \
+	treble-overlay-huawei-telephony \
 	treble-overlay-lenovo-k5pro \
-    treble-overlay-lenovo-l38111 \
+	treble-overlay-lenovo-l38111 \
 	treble-overlay-lenovo-s5 \
 	treble-overlay-lenovo-z5 \
 	treble-overlay-lg-g7 \
@@ -44,8 +42,6 @@ PRODUCT_PACKAGES += \
 	treble-overlay-moto-g6play \
 	treble-overlay-moto-g6plus \
 	treble-overlay-moto-g7power \
-	treble-overlay-NavBar \
-	treble-overlay-NightMode \
 	treble-overlay-nokia-b2n \
 	treble-overlay-nokia-ctl \
 	treble-overlay-nokia-drg \
@@ -57,9 +53,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-samsung-s9-systemui \
 	treble-overlay-sharp-s2 \
 	treble-overlay-sony-XZ3 \
-	treble-overlay-SystemUI-FalseLocks \
 	treble-overlay-tecno-camon-11 \
-	treble-overlay-Telephony-LTE \
 	treble-overlay-webview \
 	treble-overlay-wifi5g \
 	treble-overlay-xiaomi-mi6x \
@@ -90,8 +84,14 @@ PRODUCT_PACKAGES += \
 	treble-overlay-xiaomi-redminote6pro-systemui \
 	treble-overlay-xiaomi-redminote7 \
 	treble-overlay-xiaomi-redmis2 \
-	vendor/hardware_overlay/Huawei/Fingerprint/fingerprint.kl:system/phh/huawei/fingerprint.kl \
+
+
+PRODUCT_PACKAGES += \
+	hw-fpnav-daemon \
+
+PRODUCT_COPY_FILES += \
 	vendor/hardware_overlay/Huawei/Fingerprint/hw-fingerprint.rc:system/etc/init/hw-fingerprint.rc \
-	vendor/hardware_overlay/Huawei/Fingerprint/hw-fpnav.dex:system/phh/hw-fpnav.dex \
 	vendor/hardware_overlay/Huawei/Fingerprint/hw-fpnav:system/bin/hw-fpnav \
+	vendor/hardware_overlay/Huawei/Fingerprint/hw-fpnav.dex:system/phh/hw-fpnav.dex \
+	vendor/hardware_overlay/Huawei/Fingerprint/fingerprint.kl:system/phh/huawei/fingerprint.kl \
 
