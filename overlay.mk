d979a8db252ae85bb5a974b011cadb2aa98dad3f
Tuan9304 <phloix.ad0@gmail.com>
2020-03-21 22:10:45 +0700

    Change android:requiredSystemPropertyValue

--- overlay.mk
+++ overlay.mk
@@ -53,6 +53,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-oneplus-op6t \
 	treble-overlay-oneplus-op6t-systemui \
 	treble-overlay-razer-cheryl \
+	treble-overlay-realme-c1 \
 	treble-overlay-realme-x2pro \
 	treble-overlay-samsung-a50 \
 	treble-overlay-samsung-s10e \
