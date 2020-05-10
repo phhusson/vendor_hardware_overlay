6a031e5e60545ce80976586065299649ba028617
Loïc Yhuel <loic.yhuel@gmail.com>
2020-05-10 16:41:55 +0200

    Add Essential PH-1 overlay
    This fixes the statusbar height and notch.
    
    Generated using overlay/generate.sh using the last system image.

--- overlay.mk
+++ overlay.mk
@@ -13,6 +13,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-bq-jeice \
 	treble-overlay-caf-ims \
 	treble-overlay-devinputjack \
+	treble-overlay-essential-ph-1 \
 	treble-overlay-htc-exodus1 \
 	treble-overlay-htc-u12plus \
 	treble-overlay-huawei \
