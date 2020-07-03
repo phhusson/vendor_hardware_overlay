0247b675c49fe5efb3f748c770692a87ba9c79b5
YJC <openopentw@gmail.com>
2020-07-04 01:05:46 +0800

    Add overlay for HTC U11+

--- overlay.mk
+++ overlay.mk
@@ -15,6 +15,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-devinputjack \
 	treble-overlay-essential-ph_1 \
 	treble-overlay-htc-exodus1 \
+	treble-overlay-htc-u11plus \
 	treble-overlay-htc-u12plus \
 	treble-overlay-huawei \
 	treble-overlay-huawei-ANE \
