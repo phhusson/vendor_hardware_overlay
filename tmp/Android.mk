07575ceeff5d91fece84916bb849323f610f625c
Pierre-Hugues Husson <phh@phh.me>
2020-09-27 20:38:37 +0200

    Move overlays from /system/overlay to /system/product/overlay

--- tmp/Android.mk
+++ tmp/Android.mk
@@ -27,7 +27,7 @@ endif
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 # This will install the file in /system/etc/permissions
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)
 LOCAL_SRC_FILES := phh.txt
 LOCAL_UNINSTALLABLE_MODULE := true
 include $(BUILD_PREBUILT)
