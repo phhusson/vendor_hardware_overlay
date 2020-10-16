8e0d3f45f02ddd9416643f24a9fa25b290efd89c
Mohit <mohit2002ss@gmail.com>
2020-10-16 18:03:25 +0530

    fixed typo

--- Elephone/Solider/Android.mk
+++ Elephone/Solider/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_PACKAGE_NAME := treble-overlay-Elephone-Solider
+LOCAL_PACKAGE_NAME := treble-overlay-elephone-solider
 LOCAL_MODULE_PATH := $(TARGET_OUT)/overlay
 LOCAL_IS_RUNTIME_RESOURCE_OVERLAY := true
 LOCAL_PRIVATE_PLATFORM_APIS := true
