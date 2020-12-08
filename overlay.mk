21c4a2f95d0143c53444436a4c1c635a658f2894
TuxKruger <tuxkruger@github.com>
2020-12-08 11:35:28 +0530

    Correctly sort RedmiNote9s entry

--- overlay.mk
+++ overlay.mk
@@ -125,9 +125,9 @@ PRODUCT_PACKAGES += \
 	treble-overlay-xiaomi-redminote7 \
 	treble-overlay-xiaomi-redminote8pro \
 	treble-overlay-xiaomi-redmis2 \
-	treble-overlay-xiaomi-redminote9s \
 	treble-overlay-xiaomi-redminote9pro \
 	treble-overlay-xiaomi-redminote9promax \
+	treble-overlay-xiaomi-redminote9s \
 
 
 PRODUCT_PACKAGES += \
