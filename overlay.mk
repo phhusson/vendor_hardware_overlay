97334106558a760ed8de08e716f84e6ba8c48b76
Ed <ed@geraghty.london>
2021-01-15 23:58:59 +0000

    Oukitel WP8 Pro Overlay
    Acting on the feedback from the previous commit (which also makes it region ambivalent).
    
    Signed-off-by: Ed Geraghty <ed@geraghty.london>

--- overlay.mk
+++ overlay.mk
@@ -71,6 +71,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-oneplus-op6 \
 	treble-overlay-oneplus-op6t \
 	treble-overlay-oneplus-op6t-systemui \
+	treble-overlay-oukitel-wp8pro \
 	treble-overlay-razer-cheryl \
 	treble-overlay-realme-6 \
 	treble-overlay-realme-6i \
