71c719bb2fdc3501db8198a19d85555f5b8a5dc1
needmypandora <73958831+needmypandora@users.noreply.github.com>
2021-03-08 00:20:20 +0300

    Update Android.mk

--- Samsung/J7P/Android.mk
+++ Samsung/J7P/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_PACKAGE_NAME := treble-overlay-samsung-j7p
+LOCAL_PACKAGE_NAME := treble-overlay-samsung-j7prime
 LOCAL_MODULE_PATH := $(TARGET_OUT)/overlay
 LOCAL_IS_RUNTIME_RESOURCE_OVERLAY := true
 LOCAL_PRIVATE_PLATFORM_APIS := true
