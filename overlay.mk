3cbf12d8ca94018e4e890c17803d30424cd6806f
LegacyGamerHD <wesleyvanneck1012@gmail.com>
2021-06-06 12:17:13 +0200

    fix

--- overlay.mk
+++ overlay.mk
@@ -60,6 +60,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-moto-g8power \
 	treble-overlay-moto-one \
 	treble-overlay-moto-onehyper \
+	treble-overlay-moto-g5gplus \
 	treble-overlay-mtk-ims \
 	treble-overlay-nokia-b2n-7plus \
 	treble-overlay-nokia-ctl-7-1 \
