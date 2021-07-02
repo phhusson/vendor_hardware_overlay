02ecae04345553e04abf0afa89ff05b01d60b2b6
Adil <adilh+github@orchid.anonaddy.com>
2021-07-02 04:33:32 +0100

    Add overlay for r8s (Samsung Galaxy S20 FE 4G Exynos)

--- overlay.mk
+++ overlay.mk
@@ -107,6 +107,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-samsung-gta3xl \
 	treble-overlay-samsung-j6 \
 	treble-overlay-samsung-n9q \
+	treble-overlay-samsung-r8s \
 	treble-overlay-samsung-s10e \
 	treble-overlay-samsung-s9pq \
 	treble-overlay-samsung-s9q \
