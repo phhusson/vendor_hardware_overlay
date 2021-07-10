847b9e2bc6e43b2cd2448fe15b3734f04563ee39
Andy CrossGate Yan <GeForce8800Ultra@gmail.com>
2021-07-10 14:53:21 +0000

    Add resources for MBI S10
    Change-Id: I771aadb0dc2d2e6d4f1760dd9757755d10956098

--- overlay.mk
+++ overlay.mk
@@ -55,6 +55,8 @@ PRODUCT_PACKAGES += \
 	treble-overlay-lg-g7 \
 	treble-overlay-lg-v40 \
 	treble-overlay-lg-velvet \
+	treble-overlay-mbi-s10 \
+	treble-overlay-mbi-s10-systemui \
 	treble-overlay-meizu-m2181 \
 	treble-overlay-meizu-m2181-systemui \
 	treble-overlay-misc-aod \
