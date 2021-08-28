df5d48b056661c9c40114aba60f5611810f5c424
brendanus <brendanus@gmail.com>
2021-08-28 18:48:34 +0100

    Renamed BV9600E_EEA

--- Blackview/BV9600E_EEA/Android.mk
+++ Blackview/BV9600E_EEA/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_PACKAGE_NAME := treble-overlay-Blackview-BV9600E
+LOCAL_PACKAGE_NAME := treble-overlay-Blackview-BV9600E_EEA
 LOCAL_MODULE_PATH := $(TARGET_OUT)/overlay
 LOCAL_IS_RUNTIME_RESOURCE_OVERLAY := true
 LOCAL_PRIVATE_PLATFORM_APIS := true
--- build/build.sh
+++ build/build.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-
 set -e
-
 if [ "$1" == "--local-aapt" ];then
     export LD_LIBRARY_PATH=.
     export PATH=.:$PATH
     shift
 fi
-
 script_dir="$(dirname "$(readlink -f -- "$0")")"
 if [ "$#" -eq 1 ]; then
     if [ -d "$1" ];then
@@ -20,19 +17,15 @@ else
     cd "$script_dir"
     makes="$(find "$PWD/.." -name Android.mk)"
 fi
-
 if ! command -v aapt > /dev/null;then
     export LD_LIBRARY_PATH=.
     export PATH=$PATH:.
 fi
-
 if ! command -v aapt > /dev/null;then
     echo "Please install aapt (apt install aapt should do)"
     exit 1
 fi
-
 cd "$script_dir"
-
 echo "$makes" | while read -r f;do
     name="$(sed -nE 's/LOCAL_PACKAGE_NAME.*:\=\s*(.*)/\1/p' "$f")"
     grep -q treble-overlay <<<"$name" || continue
