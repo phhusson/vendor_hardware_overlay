1085784c229bf6efabb2e04765ae5136f6509593
danielalmeida1481 <daniel58almeida@gmail.com>
2022-01-07 00:00:24 +0000

    Added MI11Lite5G

--- overlay.mk
+++ overlay.mk
@@ -162,7 +162,6 @@ PRODUCT_PACKAGES += \
 	treble-overlay-vsmart-live \
 	treble-overlay-webview \
 	treble-overlay-wifi5g \
-	treble-overlay-xiaomi-mi11lite5g \
 	treble-overlay-xiaomi-mi6x \
 	treble-overlay-xiaomi-mi8 \
 	treble-overlay-xiaomi-mi8ee \
@@ -171,6 +170,9 @@ PRODUCT_PACKAGES += \
 	treble-overlay-xiaomi-mi8se \
 	treble-overlay-xiaomi-mi9 \
 	treble-overlay-xiaomi-mi9se \
+	treble-overlay-xiaomi-mi11lite5g \
+	treble-overlay-xiaomi-mi11lite5g-settings \
+	treble-overlay-xiaomi-mi11lite5g-systemui \
 	treble-overlay-xiaomi-mia2 \
 	treble-overlay-xiaomi-mia2lite \
 	treble-overlay-xiaomi-mimax3 \
