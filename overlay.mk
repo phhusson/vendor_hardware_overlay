3231c7134734ddb25df3d189125addc5f6e72c3f
Andy CrossGate Yan <GeForce8800Ultra@gmail.com>
2022-01-12 14:31:52 +0000

    Add resources for Samsung Galaxy Note10 QCOM (d1q) & 5G (d1x)
    Change-Id: Ice84289e77da3481eaddbd48b25f592f79c5164d

--- overlay.mk
+++ overlay.mk
@@ -129,6 +129,10 @@ PRODUCT_PACKAGES += \
 	treble-overlay-samsung-a71 \
 	treble-overlay-samsung-a80 \
 	treble-overlay-samsung-a9 \
+	treble-overlay-samsung-d1q \
+	treble-overlay-samsung-d1q-systemui \
+	treble-overlay-samsung-d1x \
+	treble-overlay-samsung-d1x-systemui \
 	treble-overlay-samsung-gta3xl \
 	treble-overlay-samsung-j6 \
 	treble-overlay-samsung-n9q \
