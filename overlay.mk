74e1f88d690b8783bd1a62d552f2579ec222291c
Yillié <yilliee@protonmail.com>
2022-01-20 21:13:08 +0500

    A51 : Add SystemUI overlay
    This (status_bar_padding_top) would help to make
    status bar icons align with the hole punch cutout
    on the y-axis.

--- overlay.mk
+++ overlay.mk
@@ -124,6 +124,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-samsung-a40 \
 	treble-overlay-samsung-a50 \
 	treble-overlay-samsung-a51 \
+	treble-overlay-samsung-a51-systemui \
 	treble-overlay-samsung-a51x \
 	treble-overlay-samsung-a60q \
 	treble-overlay-samsung-a7 \
