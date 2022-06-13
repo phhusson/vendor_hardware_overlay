9f5da30c9c9bc89f9362eb19244d3e818110be4a
IAGM12 <81096789+IAGM12@users.noreply.github.com>
2022-06-13 00:39:03 -0400

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -41,5 +41,5 @@ echo "$makes" | while read -r f;do
     path="$(dirname "$f")"
     aapt package -f -F "${name}-unsigned.apk" -M "$path/AndroidManifest.xml" -S "$path/res" -I android.jar
     LD_LIBRARY_PATH=./signapk/ java -jar signapk/signapk.jar keys/platform.x509.pem keys/platform.pk8 "${name}-unsigned.apk" "${name}.apk"
-    rm "${name}-unsigned.apk"
+    rm -f "${name}-unsigned.apk"
 done
