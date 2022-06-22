1bc4bbed2fc2de37e54c627a494bf0dc9ef1f03d
Julev Ajeto <jersoncarin25@gmail.com>
2022-06-22 16:49:57 +0800

    Fix Priority level and added in overlay.mk for Poco X3 GT

--- overlay.mk
+++ overlay.mk
@@ -124,6 +124,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-xiaomi-mipad4 \
 	treble-overlay-xiaomi-miplay \
 	treble-overlay-xiaomi-pocof1 \
+	treble-overlay-xiaomi-pocox3gt \
 	treble-overlay-xiaomi-redmi6 \
 	treble-overlay-xiaomi-redmi6a \
 	treble-overlay-xiaomi-redmi6pro \
