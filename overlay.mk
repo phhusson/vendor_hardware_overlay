64f7916a045137253951a4bf6b7eab4c05a0e9ae
roynatech2544 <whiteshell2544@naver.com>
2022-07-13 20:14:59 +0900

    Fix error from CI

--- overlay.mk
+++ overlay.mk
@@ -105,12 +105,12 @@ PRODUCT_PACKAGES += \
 	treble-overlay-nokia-pnx-8.1-x7-systemui \
 	treble-overlay-nubia-redmagic3s \
 	treble-overlay-nubia-z18mini \
+	treble-overlay-oneplus-n100 \
+	treble-overlay-oneplus-n100-systemui \
 	treble-overlay-oneplus-n2 \
 	treble-overlay-oneplus-n2-systemui \
 	treble-overlay-oneplus-n20 \
 	treble-overlay-oneplus-n20-systemui \
-	treble-overlay-oneplus-n100 \
-	treble-overlay-oneplus-n100-systemui \
 	treble-overlay-oneplus-n200 \
 	treble-overlay-oneplus-n200-systemui \
 	treble-overlay-oneplus-op10pro \
