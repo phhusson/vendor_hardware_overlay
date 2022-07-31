90a0d6511d96468789c3cad3cda152267aa2ef64
Zillion <77107077+DevZillion@users.noreply.github.com>
2022-07-31 11:15:09 +0300

    Added A31 Overlays

--- overlay.mk
+++ overlay.mk
@@ -151,6 +151,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-samsung-a20s \
 	treble-overlay-samsung-a20s-systemui \
 	treble-overlay-samsung-a30 \
+	treble-overlay-samsung-a31 \
 	treble-overlay-samsung-a33 \
 	treble-overlay-samsung-a33-systemui \
 	treble-overlay-samsung-a40 \
