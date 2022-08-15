c29fcf7df5980bf6dbf3432e855c15825ebf975a
cangkurileung <cangkurileungxyz@gmail.com>
2022-08-16 00:08:56 +0700

    Add overlay for Redmi9

--- overlay.mk
+++ overlay.mk
@@ -262,6 +262,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-xiaomi-redmi7onc \
 	treble-overlay-xiaomi-redmi8 \
 	treble-overlay-xiaomi-redmi8a \
+	treble-overlay-xiaomi-redmi9 \
 	treble-overlay-xiaomi-redmi9a \
 	treble-overlay-xiaomi-redmi9t \
 	treble-overlay-xiaomi-redmigo \
