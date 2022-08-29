4129c6b26152e7519d7617b74402f2ad43c52071
Deivu <davexdme@gmail.com>
2022-08-29 20:39:49 +0800

    fix: add overlay on overlay mk

--- overlay.mk
+++ overlay.mk
@@ -265,6 +265,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-xiaomi-pocom4pro5g \
 	treble-overlay-xiaomi-pocox3gt \
 	treble-overlay-xiaomi-redmi10x5g \
+	treble-overlay-xiaomi-redmi10x5g-systemui \
 	treble-overlay-xiaomi-redmi6 \
 	treble-overlay-xiaomi-redmi6a \
 	treble-overlay-xiaomi-redmi6pro \
