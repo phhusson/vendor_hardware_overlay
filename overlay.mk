d417830ec4831bc62cc2720fb1ddc88120dca74e
Andy CrossGate Yan <GeForce8800Ultra@gmail.com>
2022-10-17 00:34:27 +0000

    Add resources for Motorola Moto S30 Pro / Edge 30 Fusion (tundra)
    Change-Id: Ia98d90bd8057140db6f9004f9c0ee27d9a281c42

--- overlay.mk
+++ overlay.mk
@@ -97,6 +97,8 @@ PRODUCT_PACKAGES += \
 	treble-overlay-moto-razr \
 	treble-overlay-moto-rhodep \
 	treble-overlay-moto-rhodep-systemui \
+	treble-overlay-moto-tundra \
+	treble-overlay-moto-tundra-systemui \
 	treble-overlay-mtk-ims \
 	treble-overlay-nokia-b2n-7plus \
 	treble-overlay-nokia-ctl-7-1 \
