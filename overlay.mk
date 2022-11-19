8cdbbe350e815f991ce5efc83b63d1f8ab6c75b3
captaincookie_200 <syedmusicrealise@gmail.com>
2022-11-19 10:17:58 +0500

    Remove A32 system ui because it did nothing.

--- overlay.mk
+++ overlay.mk
@@ -180,7 +180,6 @@ PRODUCT_PACKAGES += \
 	treble-overlay-samsung-a31 \
 	treble-overlay-samsung-a31-systemui \
 	treble-overlay-samsung-a32 \
-	treble-overlay-samsung-a32-systemui \
 	treble-overlay-samsung-a33 \
 	treble-overlay-samsung-a33-systemui \
 	treble-overlay-samsung-a40 \
