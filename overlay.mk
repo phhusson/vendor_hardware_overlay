8651a575f0e5ed92589aa4dd9c2380b740c7e99a
notluke27 <77593871+notluke27@users.noreply.github.com>
2022-12-05 20:06:05 +0000

    Update overlay.mk

--- overlay.mk
+++ overlay.mk
@@ -195,6 +195,8 @@ PRODUCT_PACKAGES += \
 	treble-overlay-samsung-a7 \
 	treble-overlay-samsung-a70 \
 	treble-overlay-samsung-a71 \
+	treble-overlay-samsung-a73xq \
+        treble-overlay-samsung-a73xq-systemui \
 	treble-overlay-samsung-a80 \
 	treble-overlay-samsung-a82xq \
 	treble-overlay-samsung-a82xq-systemui \
