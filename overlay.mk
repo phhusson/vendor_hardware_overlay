35b5e1f09b55679d1c324b6aced3f74d7c461001
OneGameKoTT <i.misterok21@gmail.com>
2022-12-30 14:25:15 +0200

    Change branch

--- overlay.mk
+++ overlay.mk
@@ -147,8 +147,8 @@ PRODUCT_PACKAGES += \
 	treble-overlay-oppo-reno6-5g-systemui \
 	treble-overlay-oppo-renopro2019 \
 	treble-overlay-oppo-renopro2019-systemui \
+	treble-overlay-oukitel-c18pro \
 	treble-overlay-oukitel-wp8pro \
-	treble-overlay-oukitel-с18pro \
 	treble-overlay-razer-cheryl \
 	treble-overlay-realme-6 \
 	treble-overlay-realme-6i \
