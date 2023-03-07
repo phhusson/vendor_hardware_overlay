1bff2f4faebee6ec7616556e099609cdbdf71384
Andy CrossGate Yan <GeForce8800Ultra@gmail.com>
2023-03-07 16:11:55 +0800

    Add resources for OnePlus Ace 2V

--- overlay.mk
+++ overlay.mk
@@ -121,6 +121,8 @@ PRODUCT_PACKAGES += \
 	treble-overlay-nubia-z18mini \
 	treble-overlay-oneplus-ace2 \
 	treble-overlay-oneplus-ace2-systemui \
+	treble-overlay-oneplus-ace2v \
+	treble-overlay-oneplus-ace2v-systemui \
 	treble-overlay-oneplus-acepro \
 	treble-overlay-oneplus-acepro-systemui \
 	treble-overlay-oneplus-n10 \
