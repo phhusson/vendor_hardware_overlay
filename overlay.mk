6e2a1491b9904633e773262fae657bcc028c2113
MmmmmDonuts <88170856+MmmmmDonuts@users.noreply.github.com>
2023-03-20 12:39:23 -0400

    Sort correctly

--- overlay.mk
+++ overlay.mk
@@ -58,12 +58,12 @@ PRODUCT_PACKAGES += \
 	treble-overlay-infinix-zero6 \
 	treble-overlay-infinix-zeroxpro \
 	treble-overlay-lenovo-Q706F \
+	treble-overlay-lenovo-Y70 \
 	treble-overlay-lenovo-k5pro \
 	treble-overlay-lenovo-s5 \
 	treble-overlay-lenovo-s5pro \
 	treble-overlay-lenovo-tabv7 \
 	treble-overlay-lenovo-tabv7-systemui \
-	treble-overlay-lenovo-Y70 \
 	treble-overlay-lenovo-z5 \
 	treble-overlay-lg-g7 \
 	treble-overlay-lg-phoenix_lao_com-phoenix_sprout \
