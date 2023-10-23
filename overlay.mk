7248fad72ac1949659d594fb8fd5adc849ae9120
Le0xFF <43521247+Le0xFF@users.noreply.github.com>
2023-10-23 10:44:59 +0200

    Add overlay for Poco F4 GT (ingres)

--- overlay.mk
+++ overlay.mk
@@ -308,6 +308,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-xiaomi-pocof1 \
 	treble-overlay-xiaomi-pocof4 \
 	treble-overlay-xiaomi-pocof4-systemui \
+	treble-overlay-xiaomi-pocof4gt \
 	treble-overlay-xiaomi-pocom3pro5g \
 	treble-overlay-xiaomi-pocom4pro5g \
 	treble-overlay-xiaomi-pocom4pro5g-systemui \
