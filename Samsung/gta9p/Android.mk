ec75b1b3f6368c145e1134fc107ba53ca1496c26
YZBruh <yagizzengin73@gmail.com>
2024-11-02 17:17:27 +0300

    Add overlay for Samsung Galaxy Tab A9+ (WiFi/LTE)

---
+++ Samsung/gta9p/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_PACKAGE_NAME := treble-overlay-samsung-gta9p
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/overlay
+LOCAL_IS_RUNTIME_RESOURCE_OVERLAY := true
+LOCAL_PRIVATE_PLATFORM_APIS := true
+include $(BUILD_PACKAGE)
